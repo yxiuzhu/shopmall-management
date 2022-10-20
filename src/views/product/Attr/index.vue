1903ee10d208226751bc92931ea437a230f19ff3
y_xiuzhu <y_xiuzhu@163.com>
2022-10-20 23:13:47 +0800

    polish: 【平台属性管理】添加属性值

--- src/views/product/Attr/index.vue
+++ src/views/product/Attr/index.vue
@@ -6,10 +6,126 @@
         :show="!isShowTable"
       ></CategorySelect>
     </el-card>
+    <el-card>
+      <div v-show="isShowTable">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!category3Id"
+          @click="addAttr"
+          >添加属性</el-button
+        >
+        <!-- 表格:展示平台属性 -->
+        <el-table style="width: 100%" border :data="attrList">
+          <el-table-column type="index" label="序号" width="80" align="center">
+          </el-table-column>
+          <el-table-column prop="attrName" label="属性名称" width="150">
+          </el-table-column>
+          <el-table-column prop="prop" label="属性值列表" width="width">
+            <template slot-scope="{ row, $index }">
+              <el-tag
+                type="success"
+                v-for="(attrValue, index) in row.attrValueList"
+                :key="attrValue.id"
+                style="margin: 0px 20px"
+                >{{ attrValue.valueName }}</el-tag
+              >
+            </template>
+          </el-table-column>
+          <el-table-column prop="prop" label="操作" width="150">
+            <template slot-scope="{ row, $index }">
+              <el-button
+                type="warning"
+                icon="el-icon-edit"
+                size="mini"
+                @click="updateAttr(row)"
+              ></el-button>
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+              ></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <!-- 添加属性|修改属性的结构 -->
+      <div v-show="!isShowTable">
+        <el-form :inline="true" ref="form" label-width="80px" :model="attrInfo">
+          <el-form-item label="属性名">
+            <el-input
+              placeholder="请输入属性名"
+              v-model="attrInfo.attrName"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          @click="addAttrValue"
+          :disabled="!attrInfo.attrName"
+          >添加属性值</el-button
+        >
+        <el-button @click="isShowTable = true">取消</el-button>
+        <el-table
+          style="width: 100%; margin: 20px 0px"
+          border
+          :data="attrInfo.attrValueList"
+        >
+          <el-table-column align="center" type="index" label="序号" width="80">
+          </el-table-column>
+          <el-table-column width="width" prop="prop" label="属性值名称">
+            <template slot-scope="{ row, $index }">
+              <!-- 这里结构需要用到span与input进行来回的切换 -->
+              <el-input
+                v-model="row.valueName"
+                placeholder="请输入属性值名称"
+                size="mini"
+                v-if="row.flag"
+                @blur="toLook(row)"
+                @keyup.native.enter="toLook(row)"
+                :ref="$index"
+              ></el-input>
+              <span
+                v-else
+                @click="toEdit(row, $index)"
+                style="display: block"
+                >{{ row.valueName }}</span
+              >
+            </template>
+          </el-table-column>
+          <el-table-column width="width" prop="prop" label="操作">
+            <template slot-scope="{ row, $index }">
+              <!-- 气泡确认框 -->
+              <el-popconfirm
+                :title="`确定删除${row.valueName}?`"
+                @onConfirm="deleteAttrValue($index)"
+              >
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                  slot="reference"
+                ></el-button>
+              </el-popconfirm>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-button
+          type="primary"
+          @click="addOrUpdateAttr"
+          :disabled="attrInfo.attrValueList.length < 1"
+          >保存</el-button
+        >
+        <el-button @click="isShowTable = true">取消</el-button>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script>
+// 按需引入lodash当中的深拷贝
+import cloneDeep from "lodash/cloneDeep";
 export default {
   name: 'Attr',
   data() {
@@ -17,25 +133,25 @@ export default {
       category1Id: '',
       category2Id: '',
       category3Id: '',
-      //接受平台属性的数据
+      // 接受平台属性的数据
       attrList: [],
-      //这个属性控制table表格显示与隐藏的
+      // 这个属性控制table表格显示与隐藏的
       isShowTable: true,
-      //收集新增属性|修改属性使用的
+      // 收集新增属性|修改属性使用的
       attrInfo: {
-        attrName: '', //属性名
+        attrName: '', // 属性名
         attrValueList: [
-          //属性值，因为属性值可以有多个因此用数组，每一个属性值都是一个对象需要attrId，valueName
+          // 属性值，因为属性值可以有多个因此用数组，每一个属性值都是一个对象需要attrId，valueName
         ],
-        categoryId: 0, //三级分类的id
-        categoryLevel: 3 //因为服务器也需要区分几级id
+        categoryId: 0, // 三级分类的id
+        categoryLevel: 3 // 因为服务器也需要区分几级id
       }
     }
   },
   methods: {
-    //自定义事件的回调
+    // 自定义事件的回调
     getCategoryId({ categoryId, level }) {
-      //区分三级分类相应的id，以及父组件进行存储
+      // 区分三级分类相应的id，以及父组件进行存储
       if (level == 1) {
         this.category1Id = categoryId
         this.category2Id = ''
@@ -44,14 +160,14 @@ export default {
         this.category2Id = categoryId
         this.category3Id = ''
       } else {
-        //代表三级分类的id有了
+        // 代表三级分类的id有了
         this.category3Id = categoryId
-        //发请求获取平台的属性数据
+        // 发请求获取平台的属性数据
         this.getAttrList()
       }
     },
-    //获取平台属性的数据
-    //当用户确定三级分类的数据时候，可以向服务器发请求获取平台属性进行展示
+    // 获取平台属性的数据
+    // 当用户确定三级分类的数据时候，可以向服务器发请求获取平台属性进行展示
     async getAttrList() {
       //获取分类的ID
       const { category1Id, category2Id, category3Id } = this
@@ -64,6 +180,53 @@ export default {
       if (result.code === 200) {
         this.attrList = result.data
       }
+    },
+    // 添加属性值回调
+    addAttrValue() {
+      // 向属性值的数组里面添加元素
+      // attrId：是你相应的属性的id，目前而言我们是添加属性的操作，还没有相应的属性的id，目前而言带给服务器的id为undefined
+      // valueName:相应的属性值的名称
+      this.attrInfo.attrValueList.push({
+        attrId: this.attrInfo.id, // 对于修改某一个属性的时候，可以在已有的属性值基础之上新增新的属性值（新增属性值的时候，需要把已有的属性的id带上）
+        valueName: '',
+        flag: true
+      })
+      // flag属性：给每一个属性值添加一个标记flag，用户切换查看模式与编辑模式，好处，每一个属性值可以控制自己的模式切换
+      // 当前flag属性，响应式数据（数据变化视图跟着变化）
+      this.$nextTick(() => {
+        this.$refs[this.attrInfo.attrValueList.length - 1].focus()
+      })
+    },
+    // 添加属性按钮的回调
+    addAttr() {
+      // 切换table显示与隐藏
+      this.isShowTable = false
+      // 清除数据
+      // 收集三级分类的id
+      this.attrInfo = {
+        attrName: '', // 属性名
+        attrValueList: [
+          // 属性值，因为属性值可以有多个因此用数组，每一个属性值都是一个对象需要attrId，valueName
+        ],
+        categoryId: this.category3Id, // 三级分类的id
+        categoryLevel: 3 // 因为服务器也需要区分几级id
+      }
+    },
+    // 修改某一个属性
+    updateAttr(row) {
+      // isShowTable变为false
+      this.isShowTable = false
+      // 将选中的属性赋值给attrInfo
+      // 由于数据结构当中存在对象里面套数组，数组里面有套对象，因此需要使用深拷贝解决这类问题
+      // 深拷贝，浅拷贝在面试的时候出现频率很高，切记达到手写深拷贝与浅拷贝
+      this.attrInfo = cloneDeep(row)
+      // 在修改某一个属性的时候，将相应的属性值元素添加上flag这个标记
+      this.attrInfo.attrValueList.forEach(item => {
+        // 这样书写也可以给属性值添加flag自动，但是会发现视图不会跟着变化（因为flag不是响应式数据）
+        // 因为Vue无法探测普通的新增 property,这样书写的属性并非响应式属性（数据变化视图跟这边）
+        // 第一个参数:对象  第二个参数:添加新的响应式属性  第三参数：新的属性的属性值
+        this.$set(item, 'flag', false)
+      })
     }
   }
 }
