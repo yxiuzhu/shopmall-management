846f6abef51599e9488bf0bdc18a373382c8db05
y_xiuzhu <y_xiuzhu@163.com>
2022-10-27 00:49:24 +0800

    polish: 【Spu管理】组件

--- src/views/product/Spu/SpuForm/index.vue
+++ src/views/product/Spu/SpuForm/index.vue
@@ -23,7 +23,7 @@
         ></el-input>
       </el-form-item>
       <el-form-item label="SPU图片">
-        <!-- 上传图片：action图片上传的地址  list-type: 文件列表的类型 on-preview:图片预览的时候会出发  on-remove:当删除图片的时候会出发 
+        <!-- 上传图片：action图片上传的地址  list-type: 文件列表的类型 on-preview:图片预览的时候会出发  on-remove:当删除图片的时候会出发
          file-list：照片墙需要展示的数据【数组：数组里面的元素务必要有name、url属性】
          on-preview：图片预览功能
          on-remove:删除图片的时候会触发
@@ -201,18 +201,18 @@ export default {
     async initSpuData(spu) {
       //获取SPU信息的数据
       let spuResult = await this.$API.spu.reqSpu(spu.id);
-      if (spuResult.code == 200) {
+      if (spuResult.code === 200) {
         //在修改spu的时候,需要向服务器发请求的，把服务器返回的数据（对象），赋值给spu属性
         this.spu = spuResult.data;
       }
       //获取品牌的信息
       let tradeMarkResult = await this.$API.spu.reqTradeMarkList();
-      if (tradeMarkResult.code == 200) {
+      if (tradeMarkResult.code === 200) {
         this.tradeMarkList = tradeMarkResult.data;
       }
       //获取spu图片的数据
       let spuImageResult = await this.$API.spu.reqSpuImageList(spu.id);
-      if (spuImageResult.code == 200) {
+      if (spuImageResult.code === 200) {
         let listArr = spuImageResult.data;
         //由于照片墙显示图片的数据需要数组，数组里面的元素需要有name与url字段
         //需要把服务器返回的数据进行修改
@@ -225,7 +225,7 @@ export default {
       }
       //获取平台全部的销售属性
       let saleResult = await this.$API.spu.reqBaseSaleAttrList();
-      if (saleResult.code == 200) {
+      if (saleResult.code === 200) {
         this.saleAttrList = saleResult.data;
       }
     },
