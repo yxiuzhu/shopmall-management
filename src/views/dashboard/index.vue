fb77439db4fba17f50fcbafc3519a94c35f1cb3f
y_xiuzhu <y_xiuzhu@163.com>
2022-11-09 23:59:18 +0800

    polish: 【首页】

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,18 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+  <div>
+  <Card></Card>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
+import Card from "./Card";
 export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
-  }
-}
+  name: "",
+  components: {
+    Card
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
+<style scoped>
 </style>
